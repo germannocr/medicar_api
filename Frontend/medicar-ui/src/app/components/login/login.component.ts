77c8153a1e9a8bcf91802d05cd01b9b8b25322c6
Germanno Correia <germanno.correia@simbioseventures.com>
2020-09-28 19:34:54 -0300

    Remove invalid files in project

--- Frontend/medicar-ui/src/app/components/login/login.component.ts
+++ Frontend/medicar-ui/src/app/components/login/login.component.ts
@@ -12,14 +12,12 @@ export class LoginComponent implements OnInit {
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
-  roles: string[] = [];
 
   constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
     if (this.tokenStorage.getToken()) {
       this.isLoggedIn = true;
-      this.roles = this.tokenStorage.getUser().roles;
     }
   }
 
@@ -31,7 +29,6 @@ export class LoginComponent implements OnInit {
 
         this.isLoginFailed = false;
         this.isLoggedIn = true;
-        this.roles = this.tokenStorage.getUser().roles;
         this.reloadPage();
       },
       err => {
